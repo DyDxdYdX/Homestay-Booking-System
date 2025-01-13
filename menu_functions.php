5f078da3ad36afe58b707f82e565ab1904145791
monazmi23 <165107315+monazmi23@users.noreply.github.com>
2025-01-13 19:23:31 +0800

    Create menu_functions.php

---
+++ menu_functions.php
@@ -0,0 +1,16 @@
+<?php
+function getMenu($usertype) {
+    switch ($usertype) {
+        case "1":
+            include 'logged_menu_admin.php';
+            break;
+        case "2":
+            include 'logged_menu_homeowner.php';
+            break;
+        default:
+            include 'logged_menu_customer.php';
+            break;
+    }
+}
+?>
+
