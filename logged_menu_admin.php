b939759d1f913b52df913ef2afe85f817b962d78
monazmi23 <165107315+monazmi23@users.noreply.github.com>
2025-01-13 20:59:16 +0800

    Update logged_menu_admin.php

--- logged_menu_admin.php
+++ logged_menu_admin.php
@@ -1,22 +1,29 @@
 <?php
-$current_page = basename($_SERVER['PHP_SELF']);
+namespace App\Menus;
 
-function isProfilePage() {
-    global $current_page;
-    return $current_page == 'profile.php' || $current_page == 'profile_edit.php';
+class AdminMenu {
+    private $currentPage;
+
+    public function __construct() {
+        $this->currentPage = basename($_SERVER['PHP_SELF']);
+    }
+
+    private function isProfilePage(): bool {
+        return in_array($this->currentPage, ['profile.php', 'profile_edit.php']);
+    }
+
+    private function isNewsPage(): bool {
+        return in_array($this->currentPage, ['news.php', 'news_edit.php']);
+    }
+
+    public function render() {
+        echo '<div class="nav" id="myTopnav">
+            <a href="index.php" class="' . ($this->currentPage === 'index.php' ? 'active' : '') . '"><i class="fa fa-fw fa-home"></i> Home</a>
+            <a href="profile.php" class="' . ($this->isProfilePage() ? 'active' : '') . '"><i class="fa fa-user" aria-hidden="true"></i> Profile</a>
+            <a href="report.php" class="' . ($this->currentPage === 'report.php' ? 'active' : '') . '"><i class="fa fa-book" aria-hidden="true"></i> Report</a>
+            <a href="user_manage.php" class="' . ($this->currentPage === 'user_manage.php' ? 'active' : '') . '"><i class="fa fa-pencil-square" aria-hidden="true"></i> Manage User</a>
+            <a href="news.php" class="' . ($this->isNewsPage() ? 'active' : '') . '"><i class="fa fa-pencil-square" aria-hidden="true"></i> News</a>
+            <a href="logout.php" onClick="return confirm(\'Logout?\');"><i class="fa fa-sign-out" aria-hidden="true"></i> Logout</a>
+        </div>';
+    }
 }
-
-function isNewsPage() {
-    global $current_page;
-    return $current_page == 'news.php' || $current_page == 'news_edit.php';
-}
-
-echo '<div class="nav" id="myTopnav">
-    <a href="index.php" class="' . ($current_page == 'index.php' ? 'active' : '') . '"><i class="fa fa-fw fa-home"></i> Home</a>
-    <a href="profile.php" class="' . (isProfilePage() ? 'active' : '') . '"><i class="fa fa-user" aria-hidden="true"></i> Profile</a>
-    <a href="report.php" class="' . ($current_page == 'report.php' ? 'active' : '') . '"><i class="fa fa-book" aria-hidden="true"></i> Report</a>
-    <a href="user_manage.php" class="' . ($current_page == 'user_manage.php' ? 'active' : '') . '"><i class="fa fa-pencil-square" aria-hidden="true"></i> Manage User</a>
-    <a href="news.php" class="' . (isNewsPage() ? 'active' : '') . '"><i class="fa fa-pencil-square" aria-hidden="true"></i> News</a>
-    <a href="logout.php" onClick="return confirm(\'Logout?\');"><i class="fa fa-sign-out" aria-hidden="true"></i> Logout</a>
-</div>';
-?>
