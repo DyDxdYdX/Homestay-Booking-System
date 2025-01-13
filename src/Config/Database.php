76aecfb17bae4e924872f72b6c7858c74cf7049e
CAASI02ly <isaacimmanuel50@gmail.com>
2025-01-14 00:25:54 +0800

    Create Database.php

---
+++ src/Config/Database.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Config;
+
+class Database
+{
+    public static function connect()
+    {
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "homestay";
+
+        // Create connection
+        $conn = new \mysqli($servername, $username, $password, $dbname);
+
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        return $conn;
+    }
+}
